57cfbbe10b1bc59276b91603826ece5e32c3894c
Himanshu Nirmalkar <114094406+Himanshu-2030@users.noreply.github.com>
2024-08-21 12:43:36 +0530

    domain authentication

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation'; // Import useRouter from next/navig
 import { auth, firestore } from './lib/firebase'; // Import auth from your Firebase configuration
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'; // Import GoogleAuthProvider and signInWithPopup
 import HeroShapes from './components/heroShapes';
+import GreenShapes from './components/heroShapeGreen';
 import styles from '../styles/LandingPage.module.css'; // Import your CSS module
 import '../styles/globals.css'
 
@@ -34,6 +35,7 @@ const LandingPage = () => {
 
   return (
     <div className={styles.container}>
+      <GreenShapes />
       <h1 className={styles.title}>Welcome to Our App</h1>
       <p className={styles.description}>Sign in to start chatting with our bot.</p>
       <HeroShapes />
